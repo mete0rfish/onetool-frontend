179b5e4ce443c4c16f0eb457a45ec20d4a24618a
InGwonKang <tnqkr3494@naver.com>
2024-07-11 15:11:46 +0900

    feat: MainPage1

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 const BannerContainer = styled.div`
   width: 100%;
-  height: 200px;
+  height: 343px;
+  border-radius: 16px;
   background-color: #ddd;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 1rem;
 `;
 
 const Banner = ({ text }: { text: string }) => {
