0712e71a43f2d6a11c5bcd4a76e95179e4d6ca78
조형준 <chohyungjun@johyeongjun-ui-MacBookPro.local>
2024-08-21 19:29:15 +0900

    chore:FAQ styling

--- src/pages/faq/FAQ.tsx
+++ src/pages/faq/FAQ.tsx
@@ -9,10 +9,30 @@ export const Title = styled.span`
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 50px 0;
+`;
+
+const SmallTitleRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 55px 0px 15px 10px;
+`;
+
+const SmallTitle = styled.h4`
+  font-size: 23px;
+  font-weight: 700;
+  margin: 0; /* Remove default margin */
+`;
+
+const FaqPrefix = styled.span`
+  font-size: 25px;
+  font-weight: 700;
+  color: lightgray;
+  margin-right: 8px; /* Space between FAQ and the title */
 `;
 
 const Line = styled(Link)`
+  display: flex;
+  align-items: center;
   border-bottom: 0.5px solid #3e3e3e;
   width: 900px;
   padding: 12px;
@@ -22,9 +42,28 @@ const Line = styled(Link)`
   }
 `;
 
+const QuestionPrefix = styled.span`
+  color: lightgray; /* Q의 색상 */
+  margin-right: 8px; /* Q와 질문 간의 간격 */
+  font-weight: 600;
+`;
+
 const WriteButton = styled(Link)`
   margin-top: 36px;
   margin-left: auto; /* 버튼을 오른쪽으로 이동 */
+  padding: 13px 20px 11px 20px;
+  border: 1px solid lightgrey; /* Light grey border */
+  background-color: white; /* White background */
+  border-radius: 5px; /* Rounded corners */
+  text-decoration: none;
+  color: black; /* Text color */
+  font-weight: 600;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: lightgrey; /* Change background on hover */
+    
+  }
 `;
 
 const FAQ = () => {
@@ -33,18 +72,23 @@ const FAQ = () => {
     "환불 정책에 대해 알고 싶습니다.",
     "제품 보증 기간은 어떻게 되나요?",
     "상품 재고 문의를 할 수 있는 방법은 무엇인가요?",
-  ]; //api로 불러올 내용들
+  ]; // API로 불러올 내용들
 
   return (
     <>
       <Title>문의사항</Title>
+      <SmallTitleRow>
+        <FaqPrefix>FAQ</FaqPrefix>
+        <SmallTitle>자주 묻는 질문</SmallTitle>
+      </SmallTitleRow>
       <Wrapper>
         {faqItems.map((item, index) => (
           <Line key={index} to={`/faq/${index + 1}`}>
+            <QuestionPrefix>Q</QuestionPrefix>
             {item}
           </Line>
         ))}
-        <WriteButton to={"/faq/write"}>작성하기</WriteButton>
+        <WriteButton to={"/faq/write"}>문의 작성하기</WriteButton>
       </Wrapper>
     </>
   );
